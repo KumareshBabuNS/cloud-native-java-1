9a6faa9a544d28895a82d6ea50c4d8206a359920
kamarul kawnayeen <kawnayeen.08@gmail.com>
2017-02-16 19:51:47 +0600

    start script updated for edge server

--- start.all.services.sh
+++ start.all.services.sh
@@ -3,3 +3,4 @@ mvn clean install
 ttab -w java -Xmx384m -jar config-server/target/config-server-1.0.jar
 ttab -w java -Xmx384m -jar eureka-server/target/eureka-server-1.0.jar
 ttab -w java -Xmx384m -jar hello-service/target/hello-service-1.0.jar
+ttab -w java -Xmx384m -jar edge-server/target/edge-server-1.0.jar

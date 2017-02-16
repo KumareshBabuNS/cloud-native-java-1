82aa5d9fc04a02ff17f7582968fe3467243ecdfa
kawnayeen <kawnayeen.08@gmail.com>
2017-02-16 09:41:29 +0600

    eureka server working as expected

--- eureka-server/src/main/java/com/kawnayeen/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/kawnayeen/EurekaServerApplication.java
@@ -1,7 +1,6 @@
 package com.kawnayeen;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
@@ -9,7 +8,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * Created by kawnayeen on 2/15/17.
  */
 @EnableEurekaServer
-@EnableAutoConfiguration
 @SpringBootApplication
 public class EurekaServerApplication {
     public static void main(String[] args){
